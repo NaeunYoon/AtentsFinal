240482c30c9a6d5c17132ed57561e5520283ddfb
NaeunYoon <whalebaby@naver.com>
2022-10-21 21:49:02 +0900

    Network 01

---
+++ Weekend/Weekend01/Atents_GameNetWork_01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atents_GameNetWork_01
+{
+    internal class Program
+    {
+        static Socket clientSock;
+        static string strIp = "127.0.0.1";
+        static int port = 8082;
+        static void Main(string[] args)
+        {
+            clientSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(strIp), port);
+
+            clientSock.Connect(ip);
+
+            byte[] receiveBuffer = new byte[1024];
+            clientSock.Receive(receiveBuffer);
+            string receiveMessage = Encoding.Default.GetString(receiveBuffer);
+            Console.WriteLine(receiveMessage);
+        }
+    }
+}
---
+++ Weekend/Weekend01/Atents_GameNetWork_01_Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atents_GameNetWork_01_Server
+{
+    internal class Program
+    {
+        static Socket serverSock;
+        static string  strIp = "127.0.0.1";
+        static int port = 8082;
+        static void Main(string[] args)
+        {
+            serverSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(strIp),port);
+
+            serverSock.Bind(ip);
+            Console.WriteLine("Bind");
+            serverSock.Listen(100);
+            Console.WriteLine("Listen");
+            Socket user = serverSock.Accept();
+            Console.WriteLine("Accept");
+
+            string message = "안녕하세요";
+            byte[] sendBuffer = Encoding.Default.GetBytes(message);
+            user.Send(sendBuffer);
+
+
+        }
+    }
+}
