903e9c8e2930911e0e1d56388b44147b030bc244
NaeunYoon <whalebaby@naver.com>
2022-11-02 15:37:10 +0900

    TEST

---
+++ P2P_client/Assets/Scripts/_11_02_P2P_Client.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class _11_02_P2P_Client : MonoBehaviour
+{
+    Socket clientSocket;
+    string strIP;
+    int port;
+    byte[] sendBuffer;
+    byte[] receiveBuffer;
+    private void Awake()
+    {   
+        strIP = "127.0.0.1";
+        port = 8082;
+        sendBuffer = new byte[128];
+        receiveBuffer = new byte[128];
+    }
+    void Start()
+    {
+        clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(strIP),port);
+        clientSocket.Connect(endPoint); //동기함수로 받기
+        Debug.Log("connect");
+        ResourcesRoad();
+
+    }
+    public void ResourcesRoad()
+    {
+        GameObject obj = Resources.Load<GameObject>("Cube");
+        Instantiate(obj);
+        Debug.Log("큐브 생성");
+    }
+    
+    void Update()
+    {
+        
+    }
+}
