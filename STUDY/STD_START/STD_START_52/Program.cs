4c79e3d6a6ab761465e6ea82e2880bc05e44efb5
NaeunYoon <whalebaby@naver.com>
2022-11-19 00:43:33 +0900

    interface

--- STUDY/STD_START/STD_START_52/Program.cs
+++ STUDY/STD_START/STD_START_52/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace STD_START_52
 {
+    delegate bool CompareDelegate(object arg1, object arg2);
+
+    class SortObject
+    {
+        object[] things;
+
+        public SortObject(object[] things)
+        {
+            this.things = things;
+        }
+
+        public void Sort (CompareDelegate compareMethod)
+        {
+            object tmp;
+
+            for (int i = 0; i < things.Length; i++)
+            {
+                int lowPos=i;
+
+                for (int j = i+1; j < things.Length; j++)
+                {
+                    if (compareMethod(things[j], things[lowPos]))
+                    {
+                        lowPos = j;
+                    }
+                }
+                tmp = things[lowPos];
+                things[lowPos] = things[i];
+                things[i] = tmp;
+            }
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
