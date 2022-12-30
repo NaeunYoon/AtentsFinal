d41241db2a3155edc062ebcd82a19ada9d317f30
NaeunYoon <whalebaby@naver.com>
2022-12-30 23:50:47 +0900

    12.24 (뒷부분)

--- Weekend/3D_Base/3D_Base/Assets/Scripts/1217/Tank/_12_17_Tank.cs
+++ Weekend/3D_Base/3D_Base/Assets/Scripts/1217/Tank/_12_17_Tank.cs
@@ -6,6 +6,11 @@ public class _12_17_Tank : MonoBehaviour
 {
     [SerializeField] private GameObject _canonBallPrefab;
     [SerializeField] private Transform _firePos;
+    //유니티는 화면 안쪽 방향이 +z 방향이다. 따라서 z와 x 평면을 움직이게 한다
+    private float _zPos = 0f;
+    private float _xPos = 0f;
+    private float _mSpeed = 10f;
+    private float _rSpeed = 50f;
     void Start()
     {
         
@@ -13,6 +18,7 @@ public class _12_17_Tank : MonoBehaviour
 
     void Update()
     {
+        #region 캐논볼 생성코드
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //키를 누름 => 입력버퍼에 키의 정보가 저장됨 => 버퍼에 눌린 키가 있는지 확인함
@@ -43,12 +49,107 @@ public class _12_17_Tank : MonoBehaviour
             // ForceMode.VelocityChange : 속도를 더하며 질량은 무시한다
 
             //캐논볼에 붙어있는 애드포스를 이용해서 포탄이 날아가게 함 ( 물리영향 + 중력의영향 = 포물선운동)
+        }
+        #endregion
+        //키보드 입력 + 이동코드 함수 호출 (키를 계속 누르고 있을 때)
+        GetKey();
+        //키 입력 후에 키 입력값을 기준으로
+        //transform.Translate(new Vector3(_xPos, 0f, _zPos).normalized * 0.04f);
+        //이동 후에 초기화 시켜준다
+        _xPos = 0f;
+        _zPos = 0f;
 
+        //============
+        GetKeyDown(); //키가 한 번 눌렸을때만 처리
+        GetKeyUp(); //키가 눌리고 뗄 때 처리
+        //=============================================
+        GetAxis();
+    }
 
+    #region GetKeyDown()
+    void GetKeyDown()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _zPos += 0.5f;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _zPos -= 0.5f;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _xPos -= 0.5f;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _xPos += 0.5f;
+        }
+    }
+    #endregion
+
+    #region GetKeyUp()
+    void GetKeyUp()
+    {
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            _zPos += 0.5f;
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            _zPos -= 0.5f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            _xPos -= 0.5f;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            _xPos += 0.5f;
+        }
+    }
+    #endregion
+
+    #region GetKey()
+    void GetKey()
+    {
+        //GetKey : Key가 눌렸을 떄
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _zPos += 0.5f;
         }
 
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _zPos -= 0.5f;
+        }
 
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _xPos -= 0.5f;
+        }
 
-
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _xPos += 0.5f;
+        }
     }
+    #endregion
+
+    #region GetKey()
+    void GetAxis()
+    {
+        float moveSpeed = _mSpeed * Time.deltaTime;   //어느 시스템에서나 동일한 움직임을 보이게 하기 위해서
+        float rotSpeed = _rSpeed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical"); //-1~1 사이의 변화량이 들어온다
+        float horizontal = Input.GetAxis("Horizontal"); //-1~1 사이의 변화량이 들어온다
+        //키를 누르면 최소 -1 부터 최대 1 사이의 변화량을 전달한다 (아무것도 안하면 0임)
+
+        // 키보드를 눌러서 변화한 변화량을 전진과 후진값으로 사용한다
+        transform.Translate(Vector3.forward * vertical * moveSpeed);
+
+        //y축을 기준으로 회전하기 때문에 up vector를 기준으로 회전시킨다
+        transform.Rotate(Vector3.up * horizontal * rotSpeed);
+    }
+    #endregion
 }
