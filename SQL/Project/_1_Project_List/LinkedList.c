f1be5dc181ef37a861648d03bed5a727bd11e628
NaeunYoon <whalebaby@naver.com>
2023-01-06 13:12:28 +0900

    DoublyLinkedList

--- SQL/Project/_1_Project_List/LinkedList.c
+++ SQL/Project/_1_Project_List/LinkedList.c
@@ -1,29 +1,32 @@
 #pragma once
 #include "LinkedList.h"
 
-//노드생성
+/*  노드 생성 */
 Node* SLL_CreateNode(ElementType NewData) 
 {
 
 	Node* NewNode = (Node*)malloc(sizeof(Node));
-	if (NewNode != NULL) {
 
+	if (NewNode != NULL) {
+		/*  데이터를 저장한다. */
 		NewNode->Data = NewData;	//데이터를 저장한다(0,1,2,3,4)
-		NewNode->NextNode = NULL;	//다음 노드에 대한 포인터는 null로 초기화한다
-									//주소값에 저장된 것이 없을 때에는 null이라고 한다
+		/*  다음 노드에 대한 포인터는 NULL로 초기화 한다. */
+		NewNode->NextNode = NULL;	
+		//주소값에 저장된 것이 없을 때에는 null이라고 한다
 		//주소값이 저장된 공간의 0은 null이다
 	}
-	return NewNode;	//노드의 주소를 반환한다
+	/*  노드의 주소를 반환한다. */
+	return NewNode;	
 }
-
+/*  노드 소멸 */
 void SLL_DestroyNode(Node* Node) 
 {
 	free(Node);
 }
-
+/*  노드 추가 */
 void SLL_AppendNode(Node** Head, Node* NewNode) 
 {	//리스트의 주소값을 받기 때문에 node**
-	//헤드 노드가 null이라면 새로운 노드가 head
+	/*  헤드 노드가 NULL이라면 새로운 노드가 Head */
 	//*Head : 메인에 있는 리스트 변수의 주소값의 포인터 연산자(*&List) -> 메인에 있는 리스트 변수
 	//메인에 있는 리스트 변수의 값이 null이냐고 물음 ( 리스트 변수는 왜 만들었냐? -> 헤드노드의 주소값을 저장하려고)
 	//메인에 있는 리스트 주소에 연결된게 아무도 없니? 라고 물음 => 붙어있는 노드가 아무것도 없으므로
@@ -31,6 +34,7 @@ void SLL_AppendNode(Node** Head, Node* NewNode)
 	if ((*Head) == NULL) {	//메인에 있는 리스트 변수가 0이니?
 		*Head = NewNode;	//헤드노드를 할당함 ( 맨 처음 만들어진 0의 값이 있는 노드가 헤드 노드가 된다)
 	}
+	/*  테일을 찾아 NewNode를 연결한다. */
 	else 
 	{	//테일을 찾아 newNode를 연결한다
 		Node* Tail = (*Head);
@@ -41,7 +45,7 @@ void SLL_AppendNode(Node** Head, Node* NewNode)
 		Tail->NextNode = NewNode;
 	}
 }
-
+/*  노드 삽입 */
 void SLL_InsertAfter(Node* Current, Node* NewNode) {
 
 	NewNode->NextNode = Current->NextNode;
@@ -59,7 +63,7 @@ void SLL_InsertNewHead(Node** Head, Node* NewHead) {
 		(*Head) = NewHead;
 	}
 }
-
+/*  노드 제거 */
 void SLL_RemoveNode(Node** Head, Node* Remove) {
 	if (*Head == Remove) 
 	{
@@ -78,7 +82,7 @@ void SLL_RemoveNode(Node** Head, Node* Remove) {
 		
 	}
 }
-
+/*  노드 탐색 */
 Node* SLL_GetNodeAt(Node* Head, int Location) 
 {
 	Node* Current = Head;
@@ -89,7 +93,7 @@ Node* SLL_GetNodeAt(Node* Head, int Location)
 	}
 	return Current;
 }
-
+/*  노드 수 세기 */
 int SLL_GetNodeCount(Node* Head) 
 {
 	int Count = 0;
--- SQL/Project/_1_Project_List/Test_LinkedList.c
+++ SQL/Project/_1_Project_List/Test_LinkedList.c
@@ -8,7 +8,7 @@ int main(void)
 	Node* List = NULL;	//헤드 노드 주소값 저장용
 	Node* Current = NULL;	//현재 노드의 주소값
 	Node* NewNode = NULL;	//새롭게 만든 노드의 주소값
-
+	/*  노드 5개 추가 */
 	for (int i = 0; i < 5; i++)	//0,1,2,3,4
 	{
 		NewNode = SLL_CreateNode(i);
@@ -20,28 +20,28 @@ int main(void)
 
 	NewNode = SLL_CreateNode(-2);
 	SLL_InsertNewHead(&List, NewNode);
-
+	/*  리스트 출력 */
 	Count = SLL_GetNodeCount(List);
 	for (int i = 0; i < Count; i++)
 	{
 		Current = SLL_GetNodeAt(List, i);
 		printf("List[%d] : %d\n", i, Current->Data);
 	}
-
+	/*  리스트의 세번째 노드 뒤에 새 노드 삽입 */
 	printf("\nInserting 3000 After [2]...\n\n");
 
 	Current = SLL_GetNodeAt(List, 2);
 	NewNode = SLL_CreateNode(3000);
 
 	SLL_InsertAfter(Current, NewNode);
-
+	/*  리스트 출력 */
 	Count = SLL_GetNodeCount(List);
 	for (int i = 0; i < Count; i++)
 	{
 		Current = SLL_GetNodeAt(List, i);
 		printf("List [%d] : %d\n", i, Current->Data);
 	}
-
+	/*  모든 노드를 메모리에서 제거     */
 	printf("\nDestroying List...\n");
 	for (int i = 0; i < Count; i++)
 	{
